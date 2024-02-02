66646b74a5b90274d0213e0f21cbe408fc7311c3
Jett Wayne <z5193169@ad.unsw.edu.au>
2024-02-02 13:36:30 +1100

    Initial setup

---
+++ setup.mjs
@@ -0,0 +1,47 @@
+export function setup(ctx) {
+    const skills = [
+        'Ranged',      // 1
+        'Mage',        // 2
+        'Prayer',      // 3
+        'Slayer',      // 4
+        'Farming',     // 5
+        'Township',    // 6
+        'Woodcutting', // 7
+        'Fishing',     // 8
+        'Firemaking',  // 9
+        'Cooking',     // 10
+        'Mining',      // 11
+        'Smithing',    // 12
+        'Thieving',    // 13
+        'Fletching',   // 14
+        'Crafting',    // 15
+        'Runecrafting',// 16
+        'Herblore',    // 17
+        'Agility',     // 18
+        'Summoning',   // 19
+        'Astrology'    // 20
+      ];
+    
+      const orderedSkills = [
+        skills[0],  // Ranged
+        skills[1],  // Mage
+        skills[13], // Fletching
+        skills[9],  // Cooking
+        skills[18], // Summoning
+        skills[12], // Thieving
+        skills[15], // Runecrafting
+        skills[16], // Herblore
+        skills[10], // Mining
+        skills[7],  // Fishing
+        skills[17], // Agility
+        skills[4],  // Farming
+        skills[19], // Astrology
+        skills[6],  // Woodcutting
+        skills[5],  // Township
+        skills[3],  // Slayer
+        skills[11], // Smithing
+        skills[14], // Crafting
+        skills[2],  // Prayer
+        skills[8]   // Firemaking
+      ];
+  }
