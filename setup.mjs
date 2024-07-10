4322163c4db1ffb6aa338025b9157b882f31e65d
Jett Wayne <z5193169@ad.unsw.edu.au>
2024-07-10 19:32:41 +1000

    Using a single location for seeting the req dungeon easier testing

--- setup.mjs
+++ setup.mjs
@@ -10,18 +10,19 @@ export async function setup(ctx) {
 
 function checkAoDLock(ctx) {
   setLevelCapIncrease();
-  let dungeonReq = game.dungeons.getObjectByID('melvorTotH:Throne_of_the_Herald');
+  const dungeon_ID = 'melvorTotH:Throne_of_the_Herald';
+  let dungeonReq = game.dungeons.getObjectByID(dungeon_ID);
   const hasDungeon = game.combat.dungeonCompletion.has(dungeonReq);
   const requirementData = {
-    dungeonID: 'melvorTotH:Throne_of_the_Herald',
+    dungeonID: dungeon_ID,
     count: 1
   };
   preventMeteorite(hasDungeon);
   addDungeonRequirementToAreas(hasDungeon, requirementData);
   preventAoDAuroras(hasDungeon, requirementData);
   if (!hasDungeon) {
-    ctx.patch(Mining, 'renderRockUnlock').replace(replaceRenderRockUnlock);
-    ctx.patch(Astrology, 'renderVisibleConstellations').replace(replaceRenderVisibleConstellations);
+    ctx.patch(Mining, 'renderRockUnlock').replace(replaceRenderRockUnlock(dungeon_ID));
+    ctx.patch(Astrology, 'renderVisibleConstellations').replace(replaceRenderVisibleConstellations(dungeon_ID));
   }
 }
 
@@ -78,97 +79,102 @@ function setLevelCapIncrease() {
   }
 }
 
-function replaceRenderRockUnlock(original) {
-  if (!this.renderQueue.rockUnlock)
-    return;
-  this.actions.forEach((rock)=>{
-      const rockMenu = rockMenus.get(rock);
-      if (rockMenu === undefined)
-          return;
-      if (rock.level > this.level || rock.abyssalLevel > this.abyssalLevel || (rock.shopItemPurchased !== undefined && !this.game.shop.isUpgradePurchased(rock.shopItemPurchased))) {
-          rockMenu.setLockedContainer(rock);
-          rockMenu.setLocked();
-      } else {
-          if (rock._namespace.name === "melvorAoD") {
-            console.log("The namespace of rock is: ", rock._namespace.name);
-            let dungeonReq = game.dungeons.getObjectByID('melvorTotH:Throne_of_the_Herald');
-            const hasDungeon = game.combat.dungeonCompletion.has(dungeonReq);
-            if (!hasDungeon) {
-              rockMenu.setLockedContainer(rock);
-              rockMenu.setLocked();
+function replaceRenderRockUnlock(dungeon_ID) {
+  return function(original) {
+
+    if (!this.renderQueue.rockUnlock)
+      return;
+    this.actions.forEach((rock)=>{
+        const rockMenu = rockMenus.get(rock);
+        if (rockMenu === undefined)
+            return;
+        if (rock.level > this.level || rock.abyssalLevel > this.abyssalLevel || (rock.shopItemPurchased !== undefined && !this.game.shop.isUpgradePurchased(rock.shopItemPurchased))) {
+            rockMenu.setLockedContainer(rock);
+            rockMenu.setLocked();
+        } else {
+            if (rock._namespace.name === "melvorAoD") {
+              console.log("The namespace of rock is: ", rock._namespace.name);
+              let dungeonReq = game.dungeons.getObjectByID(dungeon_ID);
+              const hasDungeon = game.combat.dungeonCompletion.has(dungeonReq);
+              if (!hasDungeon) {
+                rockMenu.setLockedContainer(rock);
+                rockMenu.setLocked();
+              }
+            } else {
+              rockMenu.setUnlocked();
             }
-          } else {
-            rockMenu.setUnlocked();
-          }
-      }
-      if (rock.totalMasteryRequired > 0) {
-          if (this.totalCurrentMasteryLevel > rock.totalMasteryRequired) {
-              rockMenu.hideRequirement();
-          } else {
-              rockMenu.setRequirement(templateLangString('MENU_TEXT_DRAGON_ORE_REQ', {
-                  level: numberWithCommas(rock.totalMasteryRequired)
-              }));
-          }
-      }
+        }
+        if (rock.totalMasteryRequired > 0) {
+            if (this.totalCurrentMasteryLevel > rock.totalMasteryRequired) {
+                rockMenu.hideRequirement();
+            } else {
+                rockMenu.setRequirement(templateLangString('MENU_TEXT_DRAGON_ORE_REQ', {
+                    level: numberWithCommas(rock.totalMasteryRequired)
+                }));
+            }
+        }
+    }
+    );
+    this.renderQueue.rockUnlock = false;
   }
-  );
-  this.renderQueue.rockUnlock = false;
 }
 
-function replaceRenderVisibleConstellations(original) {
-  if (!this.renderQueue.visibleConstellations)
-    return;
-  let dungeonReq = game.dungeons.getObjectByID('melvorTotH:Throne_of_the_Herald');
-  let hasDungeon = game.combat.dungeonCompletion.has(dungeonReq);
-  if (this.exploredConstellation === undefined) {
-      let lowestLocked;
-      this.actions.forEach((constellation)=>{
-          const menu = astrologyMenus.constellations.get(constellation);
-          const isInRealm = constellation.realm === this.currentRealm;
-          const isAoD = constellation._namespace.name === "melvorAoD";
-          const isUnlocked = this.level >= constellation.level && (constellation.abyssalLevel < 1 || this.abyssalLevel >= constellation.abyssalLevel);
-          if (isInRealm && !isUnlocked) {
-              if (lowestLocked === undefined || lowestLocked.level > constellation.level || lowestLocked.abyssalLevel > constellation.abyssalLevel)
-                if (!isAoD || (isAoD && hasDungeon)) {
-                  lowestLocked = constellation;
-                }  
-          }
-          if (menu === undefined)
-              return;
-          if (isInRealm && isUnlocked && (!isAoD || (isAoD && hasDungeon))) {
-              showElement(menu);
-          } else {
-              hideElement(menu);
-          }
-      }
-      );
-      if (lowestLocked === undefined) {
-          hideElement(astrologyMenus.locked);
-      } else {
-        // console.log("The name of lowest is: ", lowestLocked);
-          astrologyMenus.locked.setConstellation(lowestLocked, this);
-          showElement(astrologyMenus.locked);
-      }
-      hideElement(astrologyMenus.explorePanel);
-  } else {
-      this.actions.forEach((constellation)=>{
-          const isAoD = constellation._namespace.name === "melvorAoD";
-          const menu = astrologyMenus.constellations.get(constellation);
-          if (menu === undefined)
-              return;
-          if (constellation !== this.exploredConstellation) {
-              hideElement(menu);
-          } else {
-            if (!isAoD || (isAoD && hasDungeon)) {
-              showElement(menu);
-              menu.setExplored();
+function replaceRenderVisibleConstellations(dungeon_ID) {
+  return function(original) {
+    if (!this.renderQueue.visibleConstellations)
+      return;
+    let dungeonReq = game.dungeons.getObjectByID(dungeon_ID);
+    let hasDungeon = game.combat.dungeonCompletion.has(dungeonReq);
+    if (this.exploredConstellation === undefined) {
+        let lowestLocked;
+        this.actions.forEach((constellation)=>{
+            const menu = astrologyMenus.constellations.get(constellation);
+            const isInRealm = constellation.realm === this.currentRealm;
+            const isAoD = constellation._namespace.name === "melvorAoD";
+            const isUnlocked = this.level >= constellation.level && (constellation.abyssalLevel < 1 || this.abyssalLevel >= constellation.abyssalLevel);
+            if (isInRealm && !isUnlocked) {
+                if (lowestLocked === undefined || lowestLocked.level > constellation.level || lowestLocked.abyssalLevel > constellation.abyssalLevel)
+                  if (!isAoD || (isAoD && hasDungeon)) {
+                    lowestLocked = constellation;
+                  }  
             }
-          }
-      }
-      );
-      hideElement(astrologyMenus.locked);
-      showElement(astrologyMenus.explorePanel);
-      astrologyMenus.explorePanel.setConstellation(this.exploredConstellation);
+            if (menu === undefined)
+                return;
+            if (isInRealm && isUnlocked && (!isAoD || (isAoD && hasDungeon))) {
+                showElement(menu);
+            } else {
+                hideElement(menu);
+            }
+        }
+        );
+        if (lowestLocked === undefined) {
+            hideElement(astrologyMenus.locked);
+        } else {
+          // console.log("The name of lowest is: ", lowestLocked);
+            astrologyMenus.locked.setConstellation(lowestLocked, this);
+            showElement(astrologyMenus.locked);
+        }
+        hideElement(astrologyMenus.explorePanel);
+    } else {
+        this.actions.forEach((constellation)=>{
+            const isAoD = constellation._namespace.name === "melvorAoD";
+            const menu = astrologyMenus.constellations.get(constellation);
+            if (menu === undefined)
+                return;
+            if (constellation !== this.exploredConstellation) {
+                hideElement(menu);
+            } else {
+              if (!isAoD || (isAoD && hasDungeon)) {
+                showElement(menu);
+                menu.setExplored();
+              }
+            }
+        }
+        );
+        hideElement(astrologyMenus.locked);
+        showElement(astrologyMenus.explorePanel);
+        astrologyMenus.explorePanel.setConstellation(this.exploredConstellation);
+    }
+    this.renderQueue.visibleConstellations = false;
   }
-  this.renderQueue.visibleConstellations = false;
 }
