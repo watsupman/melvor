5455844fc59edfe5cc1395fd4428e7c07a01e3d9
Jett Wayne <z5193169@ad.unsw.edu.au>
2024-07-10 16:13:33 +1000

    Hiding mining nodes, Base game dungeon unlocks toth, lock AoD slayer areas

--- setup.mjs
+++ setup.mjs
@@ -1,47 +1,73 @@
-export function setup(ctx) {
-    const skills = [
-        'Ranged',      // 1
-        'Mage',        // 2
-        'Prayer',      // 3
-        'Slayer',      // 4
-        'Farming',     // 5
-        'Township',    // 6
-        'Woodcutting', // 7
-        'Fishing',     // 8
-        'Firemaking',  // 9
-        'Cooking',     // 10
-        'Mining',      // 11
-        'Smithing',    // 12
-        'Thieving',    // 13
-        'Fletching',   // 14
-        'Crafting',    // 15
-        'Runecrafting',// 16
-        'Herblore',    // 17
-        'Agility',     // 18
-        'Summoning',   // 19
-        'Astrology'    // 20
-      ];
+export async function setup(ctx) {
+  await ctx.gameData.addPackage('data/gamemode.json');
+  ctx.onCharacterLoaded(ctx => {
+    // Modify or hook into game objects to influence offline calculations
+    if (game.currentGamemode.localID == "ByReleaseMode") {
+      setLevelCapIncrease();
+      addDungeonRequirementToAreas("melvorAoD");
+      ctx.patch(Mining, 'renderRockUnlock').replace(replaceRenderRockUnlock);
+    }
+  });
+}
+
+// Function to add a dungeon requirement to all areas in a given namespace map
+function addDungeonRequirementToAreas(area) {
+  game.combatAreas.namespaceMaps.get(area).entries().forEach(([key, combatArea]) => {
+    const requirementData = {
+      dungeonID: 'melvorTotH:Throne_of_the_Herald',
+      count: 1
+    };
     
-      const orderedSkills = [
-        skills[0],  // Ranged
-        skills[1],  // Mage
-        skills[13], // Fletching
-        skills[9],  // Cooking
-        skills[18], // Summoning
-        skills[12], // Thieving
-        skills[15], // Runecrafting
-        skills[16], // Herblore
-        skills[10], // Mining
-        skills[7],  // Fishing
-        skills[17], // Agility
-        skills[4],  // Farming
-        skills[19], // Astrology
-        skills[6],  // Woodcutting
-        skills[5],  // Township
-        skills[3],  // Slayer
-        skills[11], // Smithing
-        skills[14], // Crafting
-        skills[2],  // Prayer
-        skills[8]   // Firemaking
-      ];
+      const requirement = new DungeonRequirement(requirementData, game);
+      combatArea._entryRequirements.push(requirement);
+  });
+}
+
+// Sets the automatic skill cap increase from completing Impending Darkness to 120
+function setLevelCapIncrease() {
+  const impendingDarknessSet = game.skillLevelCapIncreases.namespaceMaps.get('melvorAoD').get('ImpendingDarknessSet100');
+  if (impendingDarknessSet) {
+    // Iterate over the setIncreases array to update each skill value
+    impendingDarknessSet.setIncreases.forEach(skillIncrease => {
+      skillIncrease.value = 120;  // Updating the value to 120
+    });
+    // Modify the dungeon requirement
+    // let requirementSet = impendingDarknessSet.requirementSets.get(0);
+    // if (requirementSet && requirementSet.requirements && requirementSet.requirements.length > 0) {
+    //   impendingDarknessSet.requirementSets.get(0).requirements[0].dungeon = game.dungeons.getObjectByID('melvorF:Impending_Darkness');
+    // }
   }
+}
+
+function replaceRenderRockUnlock(original) {
+  if (!this.renderQueue.rockUnlock)
+    return;
+  this.actions.forEach((rock)=>{
+      const rockMenu = rockMenus.get(rock);
+      if (rockMenu === undefined)
+          return;
+      if (rock.level > this.level || rock.abyssalLevel > this.abyssalLevel || (rock.shopItemPurchased !== undefined && !this.game.shop.isUpgradePurchased(rock.shopItemPurchased))) {
+          rockMenu.setLockedContainer(rock);
+          rockMenu.setLocked();
+      } else {
+          if (rock._namespace.name == "melvorAoD") {
+            console.log("The namespace of rock is: ", rock._namespace.name);
+            rockMenu.setLockedContainer(rock);
+            rockMenu.setLocked();
+          } else {
+            rockMenu.setUnlocked();
+          }
+      }
+      if (rock.totalMasteryRequired > 0) {
+          if (this.totalCurrentMasteryLevel > rock.totalMasteryRequired) {
+              rockMenu.hideRequirement();
+          } else {
+              rockMenu.setRequirement(templateLangString('MENU_TEXT_DRAGON_ORE_REQ', {
+                  level: numberWithCommas(rock.totalMasteryRequired)
+              }));
+          }
+      }
+  }
+  );
+  this.renderQueue.rockUnlock = false;
+}
