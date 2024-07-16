2ac48b60d522b0c5479f414f15222b34a7bab7bb
Jett Wayne <z5193169@ad.unsw.edu.au>
2024-07-17 01:30:19 +1000

    Waits for dungeon or stronghold complete

--- QueueRun/setup.mjs
+++ QueueRun/setup.mjs
@@ -2,8 +2,12 @@ export async function setup(ctx) {
   await ctx.onCharacterLoaded(ctx => {
     const queueRunButton = QueueRun(ctx);
     ui.create(queueRunButton, document.getElementById('combat-enemy-options'));
-    const enemyDeathPatch = ctx.patch(BaseManager, 'onEnemyDeath');
+    const enemyDeathPatch = ctx.patch(CombatManager, 'onEnemyDeath');
     enemyDeathPatch.after(function() {return afterEnemyDeath(queueRunButton)});
+    const dungeonIncrementPatch = ctx.patch(CombatManager, 'increaseDungeonProgress');
+    dungeonIncrementPatch.before(function() {beforeProgess(queueRunButton)});
+    const strongholdIncrementPatch = ctx.patch(CombatManager, 'increaseStrongholdProgress');
+    strongholdIncrementPatch.before(function() {beforeProgess(queueRunButton)});
   });
 }
 
@@ -11,16 +15,30 @@ function QueueRun(ctx) {
   return PetiteVue.reactive({
     $template: '#queue-run',
     isSelected: false,
+    stopDungeon: false,
     queueRun() {
       this.isSelected = !this.isSelected;
+      this.stopProgress = false;
     }
   });
 }
 
 function afterEnemyDeath (queueRunButton) {
   if (queueRunButton.isSelected) {
-    queueRunButton.isSelected = false;
-    game.combat.loot.lootAll();
-    return true;
+    if (((game.combat.areaType === CombatAreaType.Dungeon || game.combat.areaType === CombatAreaType.Stronghold) 
+          && queueRunButton.stopProgress
+        ) 
+        || (!(game.combat.areaType === CombatAreaType.Dungeon) && !(game.combat.areaType === CombatAreaType.Stronghold))) {
+      queueRunButton.isSelected = false;
+      queueRunButton.stopProgress = false;
+      game.combat.loot.lootAll();
+      return true;
+    }
+  }
+}
+
+function beforeProgess(queueRunButton) {
+  if (queueRunButton.isSelected && (game.combat.areaProgress === (game.combat.selectedArea.monsters.length - 1))) {
+    queueRunButton.stopProgress = true;
   }
 }
