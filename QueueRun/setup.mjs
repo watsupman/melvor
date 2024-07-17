a9303b5729935b91a077fe467d27791a4c3681e1
Jett Wayne <z5193169@ad.unsw.edu.au>
2024-07-17 13:17:50 +1000

    Added a setting for showing the button

--- QueueRun/setup.mjs
+++ QueueRun/setup.mjs
@@ -1,7 +1,13 @@
 export async function setup(ctx) {
-  await ctx.onCharacterLoaded(ctx => {
+  await ctx.onInterfaceReady(ctx => {
+    const modSetting = ctx.settings.section('Show/Hide Button');
     const queueRunButton = QueueRun(ctx);
-    ui.create(queueRunButton, document.getElementById('combat-enemy-options'));
+    setupModSettings(ctx, modSetting, queueRunButton);
+    var show = modSetting.get('show-button');
+    queueRunButton.show = show;
+    const parent = document.getElementById('combat-enemy-options');
+    ui.create(queueRunButton, parent);
+    updateButtonVisibility(ctx, show, queueRunButton);
     const enemyDeathPatch = ctx.patch(CombatManager, 'onEnemyDeath');
     enemyDeathPatch.after(function() {return afterEnemyDeath(queueRunButton)});
     const dungeonIncrementPatch = ctx.patch(CombatManager, 'increaseDungeonProgress');
@@ -13,9 +19,10 @@ export async function setup(ctx) {
 
 function QueueRun(ctx) {
   return PetiteVue.reactive({
-    $template: '#queue-run',
+    $template: '#queue-run-template',
     isSelected: false,
     stopDungeon: false,
+    show: true,
     queueRun() {
       this.isSelected = !this.isSelected;
       this.stopProgress = false;
@@ -41,4 +48,36 @@ function beforeProgess(queueRunButton) {
   if (queueRunButton.isSelected && (game.combat.areaProgress === (game.combat.selectedArea.monsters.length - 1))) {
     queueRunButton.stopProgress = true;
   }
+}
+
+function setupModSettings(ctx, settingSection, queueRunButton) {
+  const showValue = ctx.characterStorage.getItem('show-button');
+  const isButtonVisible = showValue !== undefined ? showValue : true; // Default visibility to true
+  queueRunButton.show = isButtonVisible;
+  if (showValue === undefined) {
+    ctx.characterStorage.setItem('show-button', true);
+  }
+  settingSection.add({
+    type: 'switch',
+    name: 'show-button',
+    label: 'Show/Hide Queue Run button',
+    hint: 'Determines if the Queue Run button should appear on the combat page.',
+    default: isButtonVisible,
+    onChange: (value) => {
+      updateButtonVisibility(ctx, value, queueRunButton);
+    }
+  });
+}
+
+function updateButtonVisibility(ctx, show, queueRunButton) {
+  const parent = document.getElementById('combat-enemy-options');
+  queueRunButton.isSelected = false;
+  queueRunButton.stopProgress = false;
+  queueRunButton.show = show;
+  if (show) {
+    const existingButton = parent.querySelector('#queue-run');
+    existingButton.classList.toggle('btn-success', queueRunButton.isSelected);
+    existingButton.classList.toggle('btn-secondary', !queueRunButton.isSelected);
+  }
+  ctx.characterStorage.setItem('show-button', show);
 }
